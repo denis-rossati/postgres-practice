c9445fb183eab86bda07b451cae937bf84d2cbd0
denis-rossati <denis.rossatiramos@gmail.com>
2022-10-02 15:03:38 -0300

    Add client.end() to queries

--- src/model/Actors.ts
+++ src/model/Actors.ts
@@ -2,26 +2,30 @@ import { Client } from 'pg';
 
 export class Actors {
   public static async getAll(client: Client) {
-    return client.query(
-      `SELECT 
-                actor_id as id,
-                first_name,
-                last_name
-             FROM Actor;
-        `,
+    const actors = await client.query(
+      `SELECT actor_id as id,
+              first_name,
+              last_name
+       FROM Actor;
+      `,
     );
+
+    await client.end();
+    return actors;
   }
 
   public static async getById(client: Client, id: number) {
-    return client.query(
-      `SELECT
-                actor_id as id,
-                first_name,
-                last_name
-            FROM Actor
-            WHERE actor_id = $1
-            LIMIT 1`,
+    const actor = client.query(
+      `SELECT actor_id as id,
+              first_name,
+              last_name
+       FROM Actor
+       WHERE actor_id = $1
+       LIMIT 1`,
       [id],
     );
+
+    await client.end();
+    return actor;
   }
 }
