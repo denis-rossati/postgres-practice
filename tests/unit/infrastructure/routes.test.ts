bb4c1df88041636ea283f90065e44aed679de47b
denis-rossati <denis.rossatiramos@gmail.com>
2022-10-15 15:46:38 -0300

    Add routes unit test

---
+++ tests/unit/infrastructure/routes.test.ts
@@ -0,0 +1,36 @@
+import { Express } from 'express';
+import { Routes } from '../../../src/infrastructure/routes';
+import { Actors } from '../../../src/infrastructure/controller';
+import { Actors as actorsMiddleware } from '../../../src/infrastructure/middlewares';
+
+describe('The router', () => {
+  let routerMock: { get: jest.Mock };
+
+  beforeEach(() => {
+    routerMock = {
+      get: jest.fn(),
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should load two routes', () => {
+    Routes.loadRoutes(routerMock as unknown as Express);
+
+    expect(routerMock.get).toBeCalledTimes(2);
+  });
+
+  it('should load the correct calls for /actors route', () => {
+    Routes.loadRoutes(routerMock as unknown as Express);
+
+    expect(routerMock.get as jest.Mock).toBeCalledWith('/actors', Actors.getAll);
+  });
+
+  it('should load the correct calls for /actors/:id route', () => {
+    Routes.loadRoutes(routerMock as unknown as Express);
+
+    expect(routerMock.get as jest.Mock).toBeCalledWith('/actors/:id', actorsMiddleware.checkActorId, Actors.getById);
+  });
+});
