64a9ca6038d26f3fe3b80a3eb307667cb35f4f66
denis-rossati <denis.rossatiramos@gmail.com>
2022-10-15 14:37:00 -0300

    Add service unit tests

--- tests/unit/domain/service/Actors.test.ts
+++ tests/unit/domain/service/Actors.test.ts
@@ -1,86 +1,77 @@
 import { connection } from '../../../../src/model/connection';
-import { Actors } from '../../../../src/domain/service/Actors';
+import { Actors } from '../../../../src/domain/service';
 import { Actors as ActorsModel } from '../../../../src/model/Actors';
 
 afterAll(async () => {
   await connection.end();
 });
 
-describe('Get all actors', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
+describe('The Actors controller', () => {
+  describe('Get all actors', () => {
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it('should return all users', async () => {
+      const mockData = [
+        { id: 1, first_name: 'PETER', last_name: 'BERKMAN' },
+        { id: 2, first_name: 'JAMES', last_name: 'DEVITO' },
+        { id: 3, first_name: 'LUKES', last_name: 'SILAS' },
+      ];
+
+      jest.spyOn(ActorsModel, 'getAll').mockResolvedValueOnce(new Promise((resolve) => {
+        resolve(mockData);
+      }));
+
+      const data = await Actors.getAll();
+
+      expect(data).toEqual(mockData);
+    });
+
+    it('should be called one time with DB connection', async () => {
+      const spy = jest.spyOn(ActorsModel, 'getAll').mockResolvedValueOnce(new Promise((resolve) => {
+        resolve(null);
+      }));
+
+      await Actors.getAll();
+
+      expect(spy).toBeCalledTimes(1);
+    });
   });
 
-  it('should return all users', async () => {
-    const mockData = [
-      { id: 1, first_name: 'PETER', last_name: 'BERKMAN' },
-      { id: 2, first_name: 'JAMES', last_name: 'DEVITO' },
-      { id: 3, first_name: 'LUKES', last_name: 'SILAS' },
-    ];
+  describe('Get actor by id', () => {
+    beforeEach(() => {
+      jest.restoreAllMocks();
+    });
 
-    jest.spyOn(ActorsModel, 'getAll').mockResolvedValueOnce(new Promise((resolve) => {
-      resolve(mockData);
-    }));
+    it('should return an empty list if no actors are found', async () => {
+      jest.spyOn(ActorsModel, 'getById').mockResolvedValueOnce(new Promise((resolve) => {
+        resolve([]);
+      }));
 
-    const data = await Actors.getAll();
+      const data = await Actors.getById(1);
 
-    expect(data).toEqual(mockData);
-  });
+      expect(data).toEqual([]);
+    });
 
-  it('should be called one time with DB connection', async () => {
-    const spy = jest.spyOn(ActorsModel, 'getAll');
+    it('should return null if DB returns null', async () => {
+      jest.spyOn(ActorsModel, 'getById').mockResolvedValueOnce(new Promise((resolve) => {
+        resolve(null);
+      }));
 
-    await Actors.getAll();
+      const data = await Actors.getById(1);
 
-    expect(spy).toBeCalledTimes(1);
-  });
-});
+      expect(data).toEqual(null);
+    });
 
-describe('Get actor by id', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
-  });
+    it('should be called one time with DB connection and actor id', async () => {
+      const spy = jest.spyOn(ActorsModel, 'getById').mockResolvedValueOnce(new Promise((resolve) => {
+        resolve(null);
+      }));
 
-  it('should return a single actor', async () => {
-    const mockData = [
-      { id: 1, first_name: 'PETER', last_name: 'BERKMAN' },
-      { id: 2, first_name: 'JAMES', last_name: 'DEVITO' },
-    ];
-
-    jest.spyOn(ActorsModel, 'getById').mockReturnValueOnce(new Promise((resolve) => {
-      resolve(mockData);
-    }));
-
-    const data = await Actors.getById(1);
-
-    expect(data).toEqual(mockData[0]);
-  });
-
-  it('should return null if any actor is found', async () => {
-    jest.spyOn(ActorsModel, 'getById').mockResolvedValueOnce(new Promise((resolve) => {
-      resolve([]);
-    }));
-
-    const data = await Actors.getById(1);
-
-    expect(data).toEqual(null);
-  });
-
-  it('should return null if DB returns null', async () => {
-    jest.spyOn(ActorsModel, 'getById').mockResolvedValueOnce(new Promise((resolve) => {
-      resolve(null);
-    }));
-
-    const data = await Actors.getById(1);
-
-    expect(data).toEqual(null);
-  });
-
-  it('should be called one time with DB connection and actor id', async () => {
-    const spy = jest.spyOn(ActorsModel, 'getById');
-
-    await Actors.getById(1);
-    expect(spy).toBeCalledTimes(1);
-    expect(spy).toBeCalledWith(1);
+      await Actors.getById(1);
+      expect(spy).toBeCalledTimes(1);
+      expect(spy).toBeCalledWith(1);
+    });
   });
 });
