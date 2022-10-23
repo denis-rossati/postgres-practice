55ec8bf42e52ec73b2ff1fece6decc8813dcd2d6
denis-rossati <denis.rossatiramos@gmail.com>
2022-10-23 00:48:23 -0300

    Wip

--- src/infrastructure/shutdown/Shutdown.ts
+++ src/infrastructure/shutdown/Shutdown.ts
@@ -1,15 +1,19 @@
 // inspired by @moebius/http-graceful-shutdown, but the lib seems abandoned.
-import { Server as HttpServer } from 'http';
+import { Server as HttpServer, IncomingMessage, ServerResponse } from 'http';
 import { Server as HttpsServer } from 'https';
 
-type Server = HttpsServer | HttpsServer;
+type Server = HttpsServer | HttpServer;
+type FlaggedConnection = { isIdle: boolean, connection: Server };
+type Connections = { [key: number]: FlaggedConnection };
 
-// Keep track of connections and their status.
-// And close all idle connections and make sure to close active connections after the request is processed.
+// Keep track of connections and their status and close all idle connections and make sure to close active connections
+// after the request is processed.
 export class Shutdown {
-    connections: { [key: number]: NodeJS.Socket } = {};
+    private connections: Connections = {};
 
-    connectionId = 1;
+    private connectionId = 1;
+
+    private terminating = false;
 
     server: Server;
 
@@ -19,9 +23,64 @@ export class Shutdown {
         this.watchServer();
     }
 
-    private watchServer() {
-        this.server.on('connection', () => {
+    public reap() {
+        this.setTerminating(true);
+        this.server.close();
 
+        Object.keys(this.connections).forEach((key: string) => {
+            const connection = this.connections[Number(key)];
+
+            Shutdown.closeConnection(connection);
         });
+    }
+
+    private watchServer() {
+        this.server.on('connection', this.onConnection.bind(this));
+        this.server.on('request', this.onRequest.bind(this));
+    }
+
+    private onConnection(connection: Server) {
+        const flaggedConnection = { isIdle: true, connection };
+        const { connectionId } = this;
+
+        flaggedConnection.connection.on('close', () => delete this.connections[connectionId]);
+
+        this.connections[connectionId] = flaggedConnection;
+        this.setNextConnectionId();
+    }
+
+    private onRequest(request: IncomingMessage, response: ServerResponse) {
+        const connection = request.socket;
+        const flaggedConnection = { isIdle: false, connection };
+
+        response.on('finish', () => {
+            flaggedConnection.isIdle = true;
+
+            if (this.terminating) {
+                Shutdown.closeConnection(flaggedConnection);
+            }
+        });
+    }
+
+    private static closeConnection(request: FlaggedConnection) {
+        if (request.isIdle) {
+            const { connection } = request;
+
+            connection.destroy();
+        }
+    }
+
+    private setNextConnectionId() {
+        const nextConnectionId = this.connectionId + 1;
+
+        this.setConnectionId(nextConnectionId);
+    }
+
+    private setConnectionId(connectionId: number) {
+        this.connectionId = connectionId;
+    }
+
+    private setTerminating(isTerminating: boolean) {
+        this.terminating = isTerminating;
     }
 }
